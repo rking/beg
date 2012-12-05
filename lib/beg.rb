1a70147a086881c7b5024cc4fe96c8cc1e1edaf6
☈king <rking@sharpsaw.org>
2012-12-05 00:25:18 -0600

    Now respects Guardfile (and runs 'guard' instead)

--- lib/beg.rb
+++ lib/beg.rb
@@ -1,10 +1,31 @@
 class Beg
   def self.cli argv
-    loop do
-      system 'inotifywait', *Dir['**/*.rb']
-      cmd = %w(bundle exec rake test)
-      puts "Running #{cmd}"
-      system cmd
+    Beg.new(argv).run
+  end
+
+  def initialize argv = []; @argv = argv end
+
+  def run
+    if File.exists? 'Guardfile'
+      run_with_guardfile
+    else
+      run_poor_man_style
     end
+  end
+
+  def run_with_guardfile
+    verbose_system %w(bundle exec guard)
+  end
+
+  def run_poor_man_style
+    loop do
+      verbose_system ['inotifywait'] + Dir['**/*.rb']
+      verbose_system %w(bundle exec rake test)
+    end
+  end
+
+  def verbose_system cmd
+    puts "Running #{cmd.join ' '}"
+    system *cmd
   end
 end
--- test/beg_test.rb
+++ test/beg_test.rb
@@ -2,14 +2,45 @@ require 'test_helper'
 require 'beg'
 
 class BegTest < MiniTest::Unit::TestCase
-  def test_cli
-    def Beg.loop; yield end # Just go through once, thanks. ☺
-    sys = []
-    Beg.stub :system, -> *args { sys << args } do
-      Beg.cli []
+  def with_fake_system_interaction returns
+    beg = Beg.new
+    def beg.loop; yield end # Just go through once, thanks. ☺
+    calls = { verbose_system: [], dir_glob: [] }
+    beg.stub :verbose_system, -> *args {
+      calls[:verbose_system] << args
+    } do
+      File.stub :exists?, -> *args {
+        calls[:file_exists] = args
+        returns[:file_exists]
+      } do
+        Dir.stub :[], -> *args {
+          calls[:dir_glob] << args
+          returns[:dir_glob]
+        } do
+          yield beg
+        end
+      end
     end
-    watch_cmd, test_cmd = sys
-    assert_equal 'inotifywait', watch_cmd[0]
-    assert_equal %w(bundle exec rake test), test_cmd[0]
+    calls
+  end
+  def test_poor_mans
+    calls = with_fake_system_interaction \
+      file_exists: false,
+      dir_glob: ['lib/foo.rb'] do |beg|
+      beg.run
+    end
+    watch_cmd, test_cmd = calls[:verbose_system]
+    assert_equal 'Guardfile', calls[:file_exists][0]
+    assert_equal [%w(inotifywait lib/foo.rb)], watch_cmd
+    assert_equal [%w(bundle exec rake test)], test_cmd
+  end
+
+  def test_with_guardfile
+    calls = with_fake_system_interaction file_exists: true do |beg|
+      beg.run
+    end
+    watch_cmd, test_cmd = calls[:verbose_system]
+    assert_equal 'Guardfile', calls[:file_exists][0]
+    assert_equal [%w(bundle exec guard)], calls[:verbose_system][0]
   end
 end
